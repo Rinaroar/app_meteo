8f3f97634f861bc08cb3a633b7f3bf305b3cc4dd
Rinaroar <marina.luxin@gmail.com>
2020-01-03 16:09:36 +0100

    finishing the editable content

--- app_async_await.js
+++ app_async_await.js
@@ -1,4 +1,4 @@
-//Mot clé JS pour que FETCH ne retourne pas une promesse mais LA RESOLUTION DE CETTE PROMESSE
+// Mot clé JS pour que FETCH ne retourne pas une promesse mais LA RESOLUTION DE CETTE PROMESSE
 // --> au lieu de l'objet qui est directement renvoyé par FETCH --> le travail se fait et on me donne ce qui es trouvé lors de ce travail
 // donc AWAIT --> attend que tout une partie soit fini, pour enfin me renvoyer la dernière resolution (ex : json.ip) pour le mettre dans la CONST
 
@@ -20,8 +20,14 @@ function capitalize (str){
   return str[0].toUpperCase() + str.slice(1);
 }
 
-async function main(){
+//donne un param par défaut à la fonction main : withID
 
+async function main(withIP = true){
+  let ville; //car ne peux plus être une constante
+
+  //si on travail sur l'IP
+  //Alors allons chercher l'IP et la ville
+if (withIP) {
   const ip = await fetch('https://api.ipify.org?format=json')
   .then(result => result.json())
   .then(json => json.ip)
@@ -30,10 +36,13 @@ async function main(){
 // mais tu vas attendre que le travail des .THEN soit fait
 // et ce qui va dans ip c'est le resultat de tout ça
 
-  const ville = await fetch('https://freegeoip.app/json/' + ip)
+  ville = await fetch('https://freegeoip.app/json/' + ip)
   .then(result => result.json())
   .then (json => json.city)
 
+} else {
+  ville = document.querySelector('#ville').textContent;
+}
 
   const meteo = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=a9b6909713aeb3552978b7534d2b5a34&lang=fr&units=metric`)
   .then(result => result.json ())
@@ -72,10 +81,13 @@ ville.addEventListener('click',() => {
 
 // Bind de ENTER
 ville.addEventListener('keydown', (e) => { //fonction qui prend en parametre l'evenement
-  if(e.keyCode === 13) //seulement pour le bouton ENTER qui a le code 13 (keycode.io)
-  e.preventDefault(); //retire l'event par defaut de la touche (ici, aller à la ligne)
-  ville.contentEditable = false;  // on arrête d'éditer
-  //on rappel la fonction main ?
+  if(e.keyCode === 13) {//seulement pour le bouton ENTER qui a le code 13 (keycode.io)
+    e.preventDefault(); //retire l'event par defaut de la touche (ici, aller à la ligne)
+    ville.contentEditable = false;  // on arrête d'éditer
+
+    //on rappel la fonction main avec false pour ne pas rentrer dans la condition
+    main(false);
+  }
 })
 
 main();
